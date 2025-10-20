4d16d8dc6cd34a725b37786bc5916912ca9d42bf
Ashley Osuna <ashleyosuna@uvic.ca>
2025-10-20 08:26:10 -0700

    fixed app having a max width

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,17 +1,18 @@
 <template>
   <section class="section" id="contact">
     <h1>Let's Connect!</h1>
-    <p>
+    <p class="text-center">
       I'd love to talk about how I can contribute toward impact.com's mission of empowering
       businesses through technology and innovation.
     </p>
+    <br />
     <div class="text-center">
       <font-awesome-icon icon="fa-envelope" /> Email:
       <a href="mailto:ashleyosuna513@gmail.com">ashleyosuna513@gmail.com</a>
     </div>
     <div class="text-center">
       <font-awesome-icon icon="fa-link" />
-      github:
+      Github:
       <a href="https://github.com/ashleyosuna" target="_blank">ashleyosuna</a>
     </div>
   </section>
